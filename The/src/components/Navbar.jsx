ce282637c075ee0cfd698ff3c01668ec0c9cbffa
root <156649157+Mandla7784@users.noreply.github.com>
2026-04-05 14:52:30 +0200

    Update Navbar styles and rename 'Features' to 'Revenue' for improved navigation clarity

--- The/src/components/Navbar.jsx
+++ The/src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 function Navbar(){
     return (
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
             <a className="navbar-brand" href="#">Transaction Monitor</a>
             <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
@@ -11,7 +11,7 @@ function Navbar(){
                         <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
                     </li>
                     <li className="nav-item">
-                        <a className="nav-link" href="#">Features</a>
+                        <a className="nav-link" href="#">Revenue</a>
                     </li>
                     <li className="nav-item">
                         <a className="nav-link" href
