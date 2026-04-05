e97aaf038abd0f5666e7df01c272744092745977
root <156649157+Mandla7784@users.noreply.github.com>
2026-04-05 14:54:00 +0200

    Add ThemeProvider and Navbar styles for improved theming and navigation

--- The/src/App.jsx
+++ The/src/App.jsx
@@ -2,20 +2,20 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TransactionalTable from './components/TransactionalTable';
+import { ThemeProvider } from './components/ThemeContext';
 
 function App() {
 
 
   return (
- 
-
-    <div className="container mt-5">
-      <Navbar />
-      <TransactionalTable />
-   
-      <button className="btn btn-primary">Add New Transaction </button>
-    </div>
-  
+    <ThemeProvider>
+      <div className="container mt-5">
+        <Navbar />
+        <TransactionalTable />
+     
+        <button className="btn btn-primary">Add New Transaction </button>
+      </div>
+    </ThemeProvider>
 
   );
 
--- The/src/components/Navbar.jsx
+++ The/src/components/Navbar.jsx
@@ -1,26 +1,30 @@
+import React from 'react';
+import { FaUser } from 'react-icons/fa';
+import { MdDarkMode, MdLightMode } from 'react-icons/md';
+import { useTheme } from './ThemeContext';
+import './Navbar.css';
+
 function Navbar(){
+    const { theme, toggleTheme } = useTheme();
+
     return (
-        <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+        <nav className="dribbble-navbar">
             <a className="navbar-brand" href="#">Transaction Monitor</a>
-            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-            <div className="collapse navbar-collapse" id="navbarNav">
-                <ul className="navbar-nav">
-                    <li className="nav-item active">
-                        <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href="#">Revenue</a>
-                    </li>
-                    <li className="nav-item">
-                        <a className="nav-link" href
-                    >Pricing</a>
-                    </li>
-                </ul>
+            <ul className="nav-links">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Revenue</a></li>
+                <li><a href="#">Pricing</a></li>
+            </ul>
+            <div className="navbar-right">
+                <FaUser className="user-icon" />
+                <div className="theme-toggle" onClick={toggleTheme}>
+                    {theme === 'light' ? <MdDarkMode /> : <MdLightMode />}
+                </div>
             </div>
         </nav>
     );
 }
+
+export default Navbar
 
 export default Navbar
